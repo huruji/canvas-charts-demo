6052fbac1c2af09eee076a7969beaf016b508abd
huruji <huruji3@foxmail.com>
2020-03-07 01:29:21 +0800

    feat: 操作反馈 的pie
    Change-Id: I5cf4311dddc22aa5341ce120260c6e0fa6cd02e5

--- bar/src/lib/Chart.tsx
+++ bar/src/lib/Chart.tsx
@@ -5,6 +5,7 @@ class Chart {
   data: any
   centerX: number
   centerY: number
+  tip: HTMLElement
   constructor(container:HTMLElement) {
     this.container = container;
     this.canvas = document.createElement('canvas')
@@ -15,11 +16,32 @@ class Chart {
     this.canvas.style.height = 400 + 'px'
     this.centerX = this.canvas.width / 2
     this.centerY = this.canvas.height / 2
+    this.tip = document.createElement('div')
+    this.tip.style.cssText = `
+      position: absolute;
+      display: none;
+      background: #000;
+      color: rgb(87, 87, 87);
+      border-radius: 5px;
+      padding: 10px;
+      font-size: 12px;
+      background-color: rgba(255, 255, 255, 0.9);
+      box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 8px;
+      z-index: 99;`;
+    this.handleContainerStyle()
+  }
+
+  handleContainerStyle() {
+    const container = this.container;
+    if (!['relative', 'absolute'].includes(container.style.position)) {
+      container.style.position = 'relative'
+    }
   }
 
   init(opt:any) {
     Object.assign(this, opt)
     this.container.appendChild(this.canvas)
+    this.container.appendChild(this.tip)
     this.create()
   }
 }
--- bar/src/lib/PieChart.tsx
+++ bar/src/lib/PieChart.tsx
@@ -1,30 +1,101 @@
-import Chart from './Chart'
+import Chart from './Chart';
 
 class PieChart extends Chart {
-  constructor(container:HTMLElement){
-    super(container)
+  total: number;
+  radius: number;
+  tipNameEle: HTMLElement;
+  tipValueEle: HTMLElement;
+  constructor (container: HTMLElement) {
+    super(container);
+  }
+
+  drawSlicePie(
+    ctx: CanvasRenderingContext2D,
+    centerX: number,
+    centerY: number,
+    radius: number,
+    startAngle: number,
+    endAngle: number,
+    color: string
+  ) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.closePath()
+    ctx.fill();
+  }
+
+  draw() {
+    let startAngle = 0;
+    for (let i = 0; i < this.data.length; i++) {
+      let item = this.data[i];
+      const endAngle = (Math.PI * 2 * item.value / this.total) + startAngle;
+      this.drawSlicePie(this.ctx, this.centerX, this.centerY, this.radius, startAngle, endAngle, item.color);
+      startAngle = endAngle;
+    }
   }
 
   create() {
     const total = this.data.reduce((acc, d) => {
-      return acc + d.value
-    }, 0)
-    const radius = Math.floor(Math.min(this.canvas.height / 2, this.canvas.width / 2))
-    let startAngle = 0
-    for(let i = 0; i < this.data.length; i++) {
-      let item = this.data[i]
-      this.ctx.fillStyle = item.color
-      this.ctx.beginPath()
-      this.ctx.moveTo(this.centerX, this.centerY)
-      const endAngle = (Math.PI * 2 * item.value / total) + startAngle
-      this.ctx.arc(this.centerX, this.centerY, radius, startAngle, endAngle)
-      this.ctx.fill()
-      startAngle = endAngle
-    }
+      return acc + d.value;
+    }, 0);
+    const radius = Math.floor(Math.min(this.canvas.height / 3, this.canvas.width / 3));
+    this.total = total;
+    this.radius = radius;
+    this.draw();
+
+    this.canvas.addEventListener('mousemove', (e) => {
+      this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height)
+      let startAngle = 0;
+      e.preventDefault();
+      const { left, top } = this.canvas.getBoundingClientRect();
+      const pos = {
+        x: e.clientX - left,
+        y: e.clientY - top
+      };
+      for (let i = 0; i < this.data.length; i++) {
+        let item = this.data[i];
+        const endAngle = (Math.PI * 2 * item.value / this.total) + startAngle;
+        this.drawSlicePie(this.ctx, this.centerX, this.centerY, this.radius, startAngle, endAngle, item.color);
+        if (this.ctx.isPointInPath(pos.x * 2, pos.y * 2)) {
+          this.drawSlicePie(this.ctx, this.centerX, this.centerY, this.radius + 20, startAngle, endAngle, item.color);
+          this.showInfo(pos.x, pos.y, item.name, item.value);
+        } else {
+          this.drawSlicePie(this.ctx, this.centerX, this.centerY, this.radius, startAngle, endAngle, item.color);
+        }
+        startAngle = endAngle;
+      }
+    });
   }
 
-  initData() {
+  hideInfo() {
+    this.tip.style.display = 'none';
+  }
 
+  showInfo(x: number, y: number, name: string, value: string) {
+    if (!this.tipNameEle) {
+      let p = document.createElement('p');
+      p.style.display = 'flex';
+      const nameEle = document.createElement('span');
+      const valueEle = document.createElement('span');
+      this.tipNameEle = nameEle;
+      this.tipValueEle = valueEle;
+      valueEle.style.marginLeft = '30px';
+      p.appendChild(nameEle);
+      p.appendChild(valueEle);
+      this.tip.appendChild(p);
+    }
+    this.tipNameEle.textContent = name;
+    this.tipValueEle.textContent = value;
+    this.tip.style.display = 'flex';
+    this.tip.style.top = y + 'px';
+    this.tip.style.left = x + 'px';
+  }
+  initData() {
+    for (var i = 0; i < this.data.length; i++) {
+
+    }
   }
 }
 
